bbb7073f2d4c80c609631d3d156748aa3f8fdb28
Darin Webster <darinleenavy@gmail.com>
2019-08-22 15:31:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,25 +23,3 @@ def find_the_cheese(array)
     cheese_types.find {|cheese| cheese == word}
   end
 end
-
-
-
-
-
-
-# def roll_call_dwarves# code an argument here
-#   # Your code here
-# end
-
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
-
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
-
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
